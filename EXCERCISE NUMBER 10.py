bebb811991f93ef54d68b74cc9d5fde431c57916
maryam atta mohammad <maryamattamohammad123@gmail.com>
2024-11-24 22:28:07 +0400

    Add files via upload

---
+++ EXCERCISE NUMBER 10.py
@@ -0,0 +1,13 @@
+ def check_even_odd(number):
+if number %2==0:
+    return"the number is even."
+else:
+return"the number is odd."
+
+main ():
+no=int(input("enter number:"))
+
+print (f" number is {result}.")
+
+if __name__==main__":
+  main()
