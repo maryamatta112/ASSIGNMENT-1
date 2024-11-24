0577badd24c3faea0079ff59893452ed72ad3445
maryam atta mohammad <maryamattamohammad123@gmail.com>
2024-11-24 22:27:25 +0400

    Add files via upload

---
+++ question no 9.py
@@ -0,0 +1,8 @@
+def hello():
+    print ('hello')# Fill the blank to print "hello" to  console
+
+def main():
+    hello()# fill the blank to call the hello() function
+
+if __name__== "_main_":
+    main()
