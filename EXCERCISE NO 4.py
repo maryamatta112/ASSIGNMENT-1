17cb689abcfc9ea0f88c4516d61a79a01c1a401b
maryam atta mohammad <maryamattamohammad123@gmail.com>
2024-11-24 22:24:01 +0400

    Add files via upload

---
+++ EXCERCISE NO 4.py
@@ -0,0 +1,8 @@
+#Ask the question to the user
+input("what is the capital of france?")
+#check users answer 
+if "answer"=="paris":   
+
+ print ("if correct! paris is the capital of france")
+else:
+ print ("the wrong ans. the correct will be paris.")
