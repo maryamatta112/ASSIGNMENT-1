868574668d78601aeed4516c3d6e2e5db9e55fda
maryam atta mohammad <maryamattamohammad123@gmail.com>
2024-11-24 22:24:24 +0400

    Add files via upload

---
+++ EXERCISE 5.cpp
@@ -0,0 +1,9 @@
+#create a dictionary that converts month month number into days
+total month _ days={ 1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31
+}
+#obtain the month number from user
+int (input("enter the number of month (1-12): "))
+verify the validity of month and output the no of days
+if 1<=month,=12:
+print(f"the month has{month-days[month]} days .2")
+else:()"the number of month is invalid.")"
