b1637d0eee591953918e9569931c148e24eb63f1
maryam atta mohammad <maryamattamohammad123@gmail.com>
2024-11-24 22:25:27 +0400

    Add files via upload

---
+++ excercise no 7.py
@@ -0,0 +1,20 @@
+#using loop 
+#count up from 50 to 0 for increments of 1
+for i in range (0,51,1,):
+    print(i)
+
+# count down from 50 to 0 in decrement of 1
+for i in range(50,-1,-1):
+    print (i)
+
+#loop the counts 30 to 50 in increment of 1 for i in range (30,51,1):
+    print(i)
+
+#count down from 50 to 10 in decrement of 2
+for i in range(50,9,-2):
+ print (i)
+#loop that count up 100 to 200 in increments of 5
+ for i in range (100,201,5):
+  print(i)
+
+
