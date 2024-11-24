15767e64c5d31ad3c381ed2230f6f134a7f062d2
maryam atta mohammad <maryamattamohammad123@gmail.com>
2024-11-24 22:23:39 +0400

    Add files via upload

---
+++ EXCERCISE 3.py
@@ -0,0 +1,13 @@
+#variables to store the information 
+name ="maryam atta "
+Hometown="ajman"
+age="18"
+
+#making the dictionary 
+info={"name":"maryam" ,"hometown":"Ajman","age":18}
+
+#outputting the values in dictionary
+
+print (info["maryam"])
+print(info["ajman"])
+print(info["18"])
